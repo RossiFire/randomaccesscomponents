6021471e55903ff8c846b5c0b1262d7865b16926
Daniele Rossino <fratsavio.dr@gmail.com>
2026-02-10 23:40:40 +0100

    feat: multi codeblocks + icons on tabs

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        primary: "bg-primary text-primary-foreground px-4 py-2 rounded-md hover:bg-primary/90 disabled:opacity-50 disabled:cursor-not-allowed",
+        primary: "bg-primary text-background px-4 py-2 rounded-md hover:bg-primary/90 disabled:opacity-50 disabled:cursor-not-allowed",
         secondary: "bg-accent cursor-pointer text-accent-foreground hover:bg-accent/70 hover:shadow-md hover:shadow-accent/50 active:scale-95 focus-visible:ring-2 focus-visible:ring-accent-foreground/30 disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none",
         ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none",
       },
--- demo/index.ts
+++ demo/index.ts
@@ -1,10 +1,12 @@
 import PageBadge from "@/components/docs/page-badge";
+import MultiCodeBlock from "@/components/docs/multi-code-block";
 import PkgInstallerTabs from "@/components/docs/pkg-installer-tabs";
 import { Nextjs, React, TailwindCSS, CSS, RadixUI, TypeScript } from "@/components/ui/tech-icons";
 import { CodeBlock } from "@/components/codeblock";
 
 export { 
     PageBadge,
+    MultiCodeBlock,
     PkgInstallerTabs,
     Nextjs,
     React,
